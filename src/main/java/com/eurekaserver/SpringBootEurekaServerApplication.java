5f216a17b15311816e94d53c704b9e3957dc8fe7
Simplice chedjou <chedjou03@yahoo.fr>
2023-01-08 15:00:44 -0600

    inital commit of the eureka service discovery server project

---
+++ src/main/java/com/eurekaserver/SpringBootEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class SpringBootEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEurekaServerApplication.class, args);
+	}
+
+}
